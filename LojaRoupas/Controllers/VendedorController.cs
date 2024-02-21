f4a51dcd407a1baf8f79fd7b4c4197e8fbdaea75
luana <luhyxi@gmail.com>
2024-02-21 13:48:06 -0300

    feat: starting controllers added

---
+++ LojaRoupas/Controllers/VendedorController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LojaRoupas.Infra;
+using LojaRoupas.Models.Users;
+
+namespace LojaRoupas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendedorController : ControllerBase
+    {
+        private readonly LojaContext _context;
+
+        public VendedorController(LojaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vendedor
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vendedor>>> GetVendedores()
+        {
+            return await _context.Vendedores.ToListAsync();
+        }
+
+        // GET: api/Vendedor/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vendedor>> GetVendedor(int id)
+        {
+            var vendedor = await _context.Vendedores.FindAsync(id);
+
+            if (vendedor == null)
+            {
+                return NotFound();
+            }
+
+            return vendedor;
+        }
+
+        // PUT: api/Vendedor/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVendedor(int id, Vendedor vendedor)
+        {
+            if (id != vendedor.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vendedor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VendedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vendedor
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Vendedor>> PostVendedor(Vendedor vendedor)
+        {
+            _context.Vendedores.Add(vendedor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVendedor", new { id = vendedor.UserId }, vendedor);
+        }
+
+        // DELETE: api/Vendedor/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVendedor(int id)
+        {
+            var vendedor = await _context.Vendedores.FindAsync(id);
+            if (vendedor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vendedores.Remove(vendedor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VendedorExists(int id)
+        {
+            return _context.Vendedores.Any(e => e.UserId == id);
+        }
+    }
+}
